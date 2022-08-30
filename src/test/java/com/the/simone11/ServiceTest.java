e67a2b84e9508e4f942c87d3fa6a3bda522137cd
DESKTOP-P4RVHFF\proconsul <simone.piccirilli@proconsul-gropu.com>
2022-08-30 17:48:22 +0200

    Junit

--- src/test/java/com/the/simone11/ServiceTest.java
+++ src/test/java/com/the/simone11/ServiceTest.java
@@ -3,7 +3,7 @@ package com.the.simone11;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.any;
 import static org.mockito.Mockito.doNothing;
-
+import static org.mockito.Mockito.doThrow;
 
 import javax.mail.internet.MimeMessage;
 
@@ -42,5 +42,20 @@ public class ServiceTest {
 		
 		assertThat(resp.getMsgResp()).isSameAs("OTS inviato con successo");
 		assertThat(resp.getEsito()).isSameAs("00");
+	}
+	
+	@Test
+	public void invioKO() {
+		GeneraInviaRequest request = new GeneraInviaRequest();
+		request.setEmail("mail");
+		request.setUsername("usrm");
+//		doNothing(javaMail.send(Mockito.any(SimpleMailMessage.class)));
+//		doNothing().doThrow(new RuntimeException()).when(javaMail).send(Mockito.any(SimpleMailMessage.class))));
+		doThrow(new RuntimeException()).when(javaMail).send( Mockito.any(SimpleMailMessage.class));
+		
+		GeneraInviaResponse resp = gis.generaeInvia(request);
+		
+		assertThat(resp.getMsgResp()).isSameAs("Errore durante invio ots via mail");
+		assertThat(resp.getEsito()).isSameAs("01");
 	}
 }
